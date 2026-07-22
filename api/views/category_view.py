f4d3b22c75b12f77b5a6759da98563c64d5e4f82
Jona <zardax.david98@gmail.com>
2026-07-22 12:00:56 -0600

    feature: Agregamos la vista para categoria

---
+++ api/views/category_view.py
@@ -0,0 +1,17 @@
+from api.models.category import Category
+from api.serializers.category_serializer import CategoryListSerializer, CategoryCreateSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategoryListSerializer
+
+    def get_queryset(self):
+        return Category.objects.filter(is_active=True)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CategoryCreateSerializer
+        elif self.action == "list":
+            return CategoryListSerializer
+        return CategoryListSerializer

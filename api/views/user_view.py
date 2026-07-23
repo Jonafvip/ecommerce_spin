7c4317e2c6916a8559e9ec465dab8f437218e90f
Jona <zardax.david98@gmail.com>
2026-07-22 21:20:54 -0600

    refactor: reorganizar la vista de usuario para mejorar la creación y recuperación de usuarios

--- api/views/user_view.py
+++ api/views/user_view.py
@@ -1,15 +1,30 @@
 from api.models.user import User
 from api.serializers.user_serializer import UserCreateSerializer, UserDetailSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class UserAPIView(ListCreateAPIView):
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+        return Response(
+            {"user": UserDetailSerializer(user).data, "token": token.key},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
     serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        return User.objects.filter(is_active=True)
-
-    def get_serializer_class(self):
-        if self.request.method == "POST":
-            return UserCreateSerializer
-        return self.serializer_class
+    def get_object(self):
+        return self.request.user

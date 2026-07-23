fdfdae0acd4257d45f3a1f72832b25b8bfa69b15
Jona <zardax.david98@gmail.com>
2026-07-22 21:24:10 -0600

    feat: Actualizar la configuración de URL para incluir las rutas de la API y la documentación de Swagger.

--- ecommerce_spin/urls.py
+++ ecommerce_spin/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path("",include("api.urls"))
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

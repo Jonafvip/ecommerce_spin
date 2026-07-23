b33b97e67b7e388f7149ad428fc99d2823ddb7f0
Jona <zardax.david98@gmail.com>
2026-07-22 21:21:45 -0600

    refactor: mejorar la estructura y permisos en OrderViewSet

--- api/views/order_view.py
+++ api/views/order_view.py
@@ -1,16 +1,39 @@
 from rest_framework import viewsets, status
 from api.models.order import Order
 from api.serializers.order_serializer import OrderListSerializer, OrderCreateSerializer
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
+from api.permissions import IsAdmin, IsCustomer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.select_related("user")
     serializer_class = OrderListSerializer
+    permission_classes = [IsAuthenticated]
+
+    serializer_actions = {
+        "list": OrderListSerializer,
+        "create": OrderCreateSerializer,
+    }
+
+    serializer_permissions = {
+        "list": [IsAuthenticated],
+        "create": [IsAuthenticated],
+        "destroy": [IsAdmin],
+    }
+
+    def get_permissions(self):
+        return self.serializer_permissions.get(self.action, super().get_permissions())
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == "ADMIN":
+            return Order.objects.all()
+        return Order.objects.filter(user=user)
 
     def get_serializer_class(self):
-        if self.action == "POST":
-            return OrderCreateSerializer
-        return OrderListSerializer
+        return self.serializer_actions.get(self.action, super().get_serializer_class())
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)

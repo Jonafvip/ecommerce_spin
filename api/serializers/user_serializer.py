acdec0e80426e82183ae509b38372d081ba1a3be
Jona <zardax.david98@gmail.com>
2026-07-22 21:21:06 -0600

    refactor: mejorar la validación y simplificar el manejo de errores en UserCreateSerializer

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -1,5 +1,6 @@
 from api.models.user import User
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -9,28 +10,24 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError({
-                "username": "Este username ya existe"
-            })
+            raise serializers.ValidationError({"username": "Este username ya existe"})
         return value
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError({
-                "email": "Este email ya existe"
-            })
+            raise serializers.ValidationError({"email": "Este email ya existe"})
         return value
 
     def validate_password(self, value):
         if len(value) < 6:
-            raise serializers.ValidationError({
-                "password": "La contraseña debe tener almenos 6 caracteres"
-            })
+            raise serializers.ValidationError(
+                {"password": "La contraseña debe tener al menos 6 caracteres"}
+            )
         return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        user.save()
+        Token.objects.create(user=user)
         return user
 
 
@@ -38,3 +35,14 @@ class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "first_name", "last_name", "email", "role"]
+
+
+class UserListAuxSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "full_name"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"

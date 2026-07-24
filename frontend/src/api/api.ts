b323f6d37183c1e6cd357ce5832169bb8138d4ec
Jona <zardax.david98@gmail.com>
2026-07-24 00:40:58 -0600

    feat: Añadir método para obtener categorías desde la API

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
   type PaginatedProductsResponse,
   type ProductListExceptCategory,
+  type CategoryList,
 } from "@/types/types";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -9,15 +10,19 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const api = {
   getProducts: async (): Promise<{ results: ProductListExceptCategory[] }> => {
     const response = await axios.get<{ results: ProductListExceptCategory[] }>(
-      BASE_URL!,
+      `${BASE_URL}products/`,
     );
     return response.data;
   },
   getProductsNavigation: async (
     url?: string,
   ): Promise<PaginatedProductsResponse> => {
-    const endpoint = url || BASE_URL!;
+    const endpoint = url || `${BASE_URL}products/`!;
     const response = await axios.get<PaginatedProductsResponse>(endpoint);
+    return response.data;
+  },
+  getCategories: async (): Promise<CategoryList[]> => {
+    const response = await axios.get<CategoryList[]>(`${BASE_URL}categories/`);
     return response.data;
   },
 };

447cab74f659c70f8c91b622ec3948ea72a6a093
Jona <zardax.david98@gmail.com>
2026-07-23 20:45:44 -0600

    feat: Añadir ruta para la página de productos en el enrutador

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Layout } from "@/layout/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
-
+import { Products } from "@/pages/Products";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
       },
     ],
   },

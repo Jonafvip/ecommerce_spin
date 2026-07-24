66bbe8b20f02dcdaee7cecc0bba5228620adeff3
Jona <zardax.david98@gmail.com>
2026-07-24 00:40:41 -0600

    feat: Añadir soporte para filtrado en la vista de productos utilizando django_filters

--- backend/api/views/product_view.py
+++ backend/api/views/product_view.py
@@ -8,12 +8,16 @@ from ..permissions import IsAdminOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from ..filters import ProductFilter
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.select_related("category").filter(is_active=True)
     serializer_class = ProductListSerializer
     permission_classes = [IsAdminOrReadOnly]
-    filter_backends = [OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = ProductFilter
     ordering_fields = ["name", "unit_price"]
     pagination_class = PageNumberPagination
     pagination_class.page_size = 9

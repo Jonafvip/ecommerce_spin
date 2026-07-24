dfdb79905bf0f2fb309bc6a27f01e224caecf337
Jona <zardax.david98@gmail.com>
2026-07-24 00:40:06 -0600

    feat: Añadir filtro de productos por rango de precios en la API

---
+++ backend/api/filters.py
@@ -0,0 +1,11 @@
+import django_filters
+from .models import Product
+
+
+class ProductFilter(django_filters.FilterSet):
+    price_min = django_filters.NumberFilter(field_name="unit_price", lookup_expr="gte")
+    price_max = django_filters.NumberFilter(field_name="unit_price", lookup_expr="lte")
+
+    class Meta:
+        model = Product
+        fields = ["unit_price"]

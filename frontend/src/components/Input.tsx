3b8e3161d6ff6489b9e3cdc34e89095026b61a64
Jona <zardax.david98@gmail.com>
2026-07-24 18:03:16 -0600

    feat: Agregue componentes Input, DropdownMenu y NativeSelect con estilo y funcionalidad.

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,37 @@
+import { Input as InputGlobal } from "@/components/ui/input";
+import { Field, FieldLabel } from "@/components/ui/field";
+
+interface InputMyProp {
+  placeholder: string;
+  label: string;
+  value: string | undefined;
+  name: string;
+  required?: boolean;
+  type?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({
+  label,
+  placeholder,
+  value,
+  name,
+  required,
+  type,
+  onChange,
+}: InputMyProp) => {
+  return (
+    <Field>
+      <FieldLabel>{label}</FieldLabel>
+      <InputGlobal
+        type={type}
+        placeholder={placeholder}
+        value={value ?? ""}
+        name={name}
+        onChange={onChange}
+        required={required}
+        className="p-5"
+      />
+    </Field>
+  );
+};

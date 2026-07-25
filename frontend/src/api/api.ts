a644ebea0dffbd0b4ae18097c3a8b5559b3b850b
Jona <zardax.david98@gmail.com>
2026-07-24 18:03:53 -0600

    feat: Agregar métodos API para detalles de productos, registro de usuarios e inicio de sesión; actualizar tipos para una mejor seguridad de tipos.

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,6 +3,10 @@ import {
   type PaginatedProductsResponse,
   type ProductListExceptCategory,
   type CategoryList,
+  type ProductDetail,
+  type RegisterUser,
+  type LoginUser,
+  type AuthTokenResponse,
 } from "@/types/types";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -11,6 +15,14 @@ export const api = {
   getProducts: async (): Promise<{ results: ProductListExceptCategory[] }> => {
     const response = await axios.get<{ results: ProductListExceptCategory[] }>(
       `${BASE_URL}products/`,
+    );
+    return response.data;
+  },
+  getProductDetail: async (
+    productId?: string | number,
+  ): Promise<ProductDetail> => {
+    const response = await axios.get<ProductDetail>(
+      `${BASE_URL}products/${productId}/`,
     );
     return response.data;
   },
@@ -23,6 +35,20 @@ export const api = {
   },
   getCategories: async (): Promise<CategoryList[]> => {
     const response = await axios.get<CategoryList[]>(`${BASE_URL}categories/`);
+    return response.data;
+  },
+  postRegister: async (userData: RegisterUser): Promise<RegisterUser> => {
+    const response = await axios.post<RegisterUser>(
+      `${BASE_URL}user/register/`,
+      userData,
+    );
+    return response.data;
+  },
+  postLogin: async (userdata: LoginUser): Promise<AuthTokenResponse> => {
+    const response = await axios.post<AuthTokenResponse>(
+      `http://127.0.0.1:8000/api-token-auth/`,
+      userdata,
+    );
     return response.data;
   },
 };
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -5,16 +5,46 @@ export interface ProductsList {
   category_id: number;
 }
 
+export interface ProductDetail {
+  id: number | string;
+  name: string;
+  description: string;
+  unit_price: string;
+  product_code: string;
+  image: string;
+  category: {
+    id: string | number;
+    name: string;
+  };
+}
+
 export type ProductListExceptCategory = Omit<ProductsList, "category">;
 
 export interface PaginatedProductsResponse {
-  count:number
+  count: number;
   next: string | null;
   previous: string | null;
   results: ProductListExceptCategory[];
 }
 
-export interface CategoryList{
-  id:number
-  name:string
+export interface CategoryList {
+  id: number;
+  name: string;
+}
+
+export interface RegisterUser {
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginUser {
+  username: string;
+  password: string;
+}
+
+export interface AuthTokenResponse {
+  token: string;
 }

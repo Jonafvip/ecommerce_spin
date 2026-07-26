0b3725ec5fd336a2d7039f02f75e6d13a4662a5d
Jona <zardax.david98@gmail.com>
2026-07-25 21:42:37 -0600

    feat: Agregar funcionalidad para eliminar artículos del carrito con manejo de errores

--- backend/api/views/cart_view.py
+++ backend/api/views/cart_view.py
@@ -1,13 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from ..models.cart import Cart
+from ..models.cart_detail import CartDetail
 from ..serializers.cart_serializer import CartCreateSerializer, CartListSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CartViewSet(ModelViewSet):
     queryset = Cart.objects.select_related("user")
     serializer_class = CartListSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     serializer_actions = {
         "list": CartListSerializer,
@@ -32,3 +36,25 @@ class CartViewSet(ModelViewSet):
             serializer.save(user=self.request.user)
         else:
             serializer.save()
+
+    @action(detail=False, methods=["delete"], url_path="remove-item")
+    def remove_item(self, request):
+        detail_id = request.data.get("detail_id")
+
+        if not detail_id:
+            return Response(
+                {"error": "Se requiere detail_id"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            item = CartDetail.objects.get(id=detail_id, cart__user=request.user)
+            item.delete()
+            return Response(
+                {"message": "Producto eliminado del carrito"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except CartDetail.DoesNotExist:
+            return Response(
+                {"error": "El detalle no existe o no te pertenece"},
+                status=status.HTTP_404_NOT_FOUND,
+            )

85147f782ea5a34646ee277df4ba4622a8a05428
Jona <zardax.david98@gmail.com>
2026-07-26 22:56:18 -0600

    feat: Integrar el conteo del carrito en el componente Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,11 @@ import { ShoppingCart } from "lucide-react";
 import { SheetSide } from "@/components/SheetResponsive";
 import { NavLink } from "react-router-dom";
 import { HoverCard } from "@/components/HoverCard";
+import { useCart } from "@/context/CartContext";
 
 export const Header = () => {
+  const { cartCount } = useCart();
+  
   return (
     <div className="flex items-center justify-between border-b border-gray-200 px-4 py-4 md:px-15 md:py-6">
       <div className="flex items-center gap-3 md:gap-0">
@@ -39,7 +42,14 @@ export const Header = () => {
         </li>
         <li className="cursor-pointer">
           <NavLink to="/cart">
-            <ShoppingCart />
+            <div className="relative">
+              {cartCount > 0 && (
+                <div className="w-5 h-5 z-10 absolute -top-4 -right-4 bg-black rounded-full flex items-center justify-center p-2">
+                  <p className="text-white">{cartCount}</p>
+                </div>
+              )}
+              <ShoppingCart />
+            </div>
           </NavLink>
         </li>
       </ul>

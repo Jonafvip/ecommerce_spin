35f28bee371cf03bf2e219a6960a0b3dbdb25952
Jona <zardax.david98@gmail.com>
2026-07-28 23:09:22 -0600

    feat: Implementar el componente Sidebar y el hook useIsMobile para mejorar la navegación y la adaptabilidad en dispositivos móviles

---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,58 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { LayoutDashboard, ShoppingCart, Archive, Users } from "lucide-react";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader className="p-4 text-xl tracking-wider">
+        Ecommerce-Spin <br />
+        <span className="text-sm text-gray-500">Panel de Admin</span>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-1">
+            <SidebarMenuItem>
+              <SidebarMenuButton className="transition-all duration-200 ease-in-out hover:bg-slate-100 dark:hover:bg-zinc-800 hover:text-primary hover:translate-x-1 py-5 pl-6">
+                <LayoutDashboard className="h-5! w-6! shrink-0" />
+                <span className="text-base font-light">Dashboard</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton className="transition-all duration-200 ease-in-out hover:bg-slate-100 dark:hover:bg-zinc-800 hover:text-primary hover:translate-x-1 py-5 pl-6">
+                <ShoppingCart className="h-5! w-6! shrink-0" />
+                <span className="text-base font-light">Orders</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton className="transition-all duration-200 ease-in-out hover:bg-slate-100 dark:hover:bg-zinc-800 hover:text-primary hover:translate-x-1 py-5 pl-6">
+                <Archive className="h-5! w-6! shrink-0" />
+                <span className="text-base font-light">Products</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton className="transition-all duration-200 ease-in-out hover:bg-slate-100 dark:hover:bg-zinc-800 hover:text-primary hover:translate-x-1 py-5 pl-6">
+                <Users className="h-5! w-6! shrink-0" />
+                <span className="text-base font-light">Customers</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter />
+    </Sidebar>
+  );
+}

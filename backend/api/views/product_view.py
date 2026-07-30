82c83778a37fe3350999ffc76c9797a1fd0e2ea2
Jona <zardax.david98@gmail.com>
2026-07-29 23:52:51 -0600

    feat: Añadir acción para obtener la lista de productos en el administrador

--- backend/api/views/product_view.py
+++ backend/api/views/product_view.py
@@ -3,13 +3,15 @@ from ..serializers.product_serializer import (
     ProductListSerializer,
     ProductCreateSerializer,
     ProductDetailSerializer,
+    ProductListAdminSeriliazer,
 )
 from ..permissions import IsAdminOrReadOnly
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, Response
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from ..filters import ProductFilter
+from rest_framework.decorators import action
 
 
 class ProductViewSet(ModelViewSet):
@@ -28,6 +30,7 @@ class ProductViewSet(ModelViewSet):
         "retrieve": ProductDetailSerializer,
         "update": ProductDetailSerializer,
         "partial_update": ProductDetailSerializer,
+        "get_list_products_admin": ProductListAdminSeriliazer,
     }
 
     def get_queryset(self):
@@ -35,3 +38,16 @@ class ProductViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         return self.serializer_actions.get(self.action, super().get_serializer_class())
+
+    @action(detail=False, methods=["get"], url_path="list-products-admin")
+    def get_list_products_admin(self, request):
+        queryset = Product.objects.select_related("category").all()
+        filter_queryset = self.filter_queryset(queryset)
+
+        page = self.paginate_queryset(filter_queryset)
+        if page is None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(filter_queryset, many=True)
+        return Response(serializer.data)

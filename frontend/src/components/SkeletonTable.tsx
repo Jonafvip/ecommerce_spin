50764209efa730773a66787579f6aa5a3629b90c
Jona <zardax.david98@gmail.com>
2026-08-05 18:52:30 -0600

    feat: Agregue el componente SkeletonTable para cargar la representación del estado

---
+++ frontend/src/components/SkeletonTable.tsx
@@ -0,0 +1,29 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const SkeletonTable = () => {
+  return (
+    <div className="w-full flex border-2 border-gray-200 rounded-md  overflow-hidden">
+      <div className="flex gap-4 bg-gray-100 px-4 py-3">
+        <Skeleton className="h-4 w-16" />
+        <Skeleton className="h-4 flex-1" />
+        <Skeleton className="h-4 w-24" />
+        <Skeleton className="h-4 w-20" />
+        <Skeleton className="h-4 w-16" />
+        <Skeleton className="h-4 w-16" />
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div
+            className="flex gap-4 border-t border-gray-100 px-4 py-4"
+            key={index}
+          >
+            <Skeleton className="h-10 w-10 rounded-md" />
+            <Skeleton className="h-4 w-1/3" />
+            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-4 w-16" />
+            <Skeleton className="h-4 w-16" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};

1c131e992043281c5dc678536593ec0c79b422fa
Jona <zardax.david98@gmail.com>
2026-08-10 18:33:46 -0600

    feat: agregar soporte para modo oscuro en el componente SkeletonTable

--- frontend/src/components/SkeletonTable.tsx
+++ frontend/src/components/SkeletonTable.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const SkeletonTable = () => {
   return (
-    <div className="w-full flex border-2 border-gray-200 rounded-md  overflow-hidden">
-      <div className="flex gap-4 bg-gray-100 px-4 py-3">
+    <div className="w-full flex border-2 border-gray-200 dark:border-zinc-800 rounded-md  overflow-hidden">
+      <div className="flex gap-4 bg-gray-100 dark:bg-zinc-900 px-4 py-3">
         <Skeleton className="h-4 w-16" />
         <Skeleton className="h-4 flex-1" />
         <Skeleton className="h-4 w-24" />
@@ -12,7 +12,7 @@ export const SkeletonTable = () => {
         <Skeleton className="h-4 w-16" />
         {Array.from({ length: 5 }).map((_, index) => (
           <div
-            className="flex gap-4 border-t border-gray-100 px-4 py-4"
+            className="flex gap-4 border-t border-gray-100 dark:border-zinc-800 px-4 py-4"
             key={index}
           >
             <Skeleton className="h-10 w-10 rounded-md" />

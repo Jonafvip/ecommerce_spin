06fa7732bb00a844c130625924ff43c08bc4bed4
Jona <zardax.david98@gmail.com>
2026-08-14 23:46:57 -0600

    feat: Actualizar los permisos en UserListByAdminAPIView para restringir el acceso solo a los administradores.

--- backend/api/views/user_view.py
+++ backend/api/views/user_view.py
@@ -11,6 +11,7 @@ from rest_framework.generics import (
     ListAPIView,
 )
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from ..permissions import IsAdmin
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
@@ -47,10 +48,11 @@ class UserRetrieveAPIView(RetrieveUpdateAPIView):
             return UserUpdateSerializer
         return UserDetailSerializer
 
+
 class UserListByAdminAPIView(ListAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserListByAdminSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdmin]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ["username"]
     pagination_class = PageNumberPagination

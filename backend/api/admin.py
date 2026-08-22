c91ec08628d7ffb1a9f58a320eb59c409bfd6f1b
Jona <zardax.david98@gmail.com>
2026-08-21 23:54:12 -0600

    feat: Registrar los modelos Cart, Category, Product, User y CartDetail en el admin de Django

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cart, Category, Product, User,CartDetail
 
 # Register your models here.
+
+admin.site.register([User, Product, Cart,CartDetail, Category])

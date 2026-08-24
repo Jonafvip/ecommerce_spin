34344379212235772f5948fed17e495cb38974b9
Jona <zardax.david98@gmail.com>
2026-08-23 23:24:40 -0600

    feat: Registrar las rutas /support, /help-centers y /about/history en el router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,9 @@ import { CartProvider } from "./context/CartContext";
 import { About } from "./pages/About";
 import { PrivacyPolicy } from "./pages/PrivacyPolicy";
 import { TermsOfService } from "./pages/TermsOfService";
+import { Support } from "./pages/Support";
+import { HelpCenters } from "./pages/HelpCenters";
+import { History } from "./pages/History";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
 import { CreateProducts } from "./pages/Dashboard/CreateProducts";
 import { Customer } from "./pages/Dashboard/Customer";
@@ -55,6 +58,18 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/about/history",
+        element: <History />,
+      },
+      {
+        path: "/support",
+        element: <Support />,
+      },
+      {
+        path: "/help-centers",
+        element: <HelpCenters />,
       },
       {
         path: "/privacy-policy",
